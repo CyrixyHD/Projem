3f4b37459fdee181dcb38118511eae06c87913de
CyrixyHD <161216784+CyrixyHD@users.noreply.github.com>
2024-02-25 16:37:04 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+            
+            
+for i in meme_dict.keys():
+    print(i)
